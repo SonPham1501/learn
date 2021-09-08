ecb6bfbb7e89070f699f3804da7ecfab4ce2136a
SonPham1501 <84057196+SonPham1501@users.noreply.github.com>
2021-09-08 20:28:45 +0700

    dsada

--- lib/config/constant.dart
+++ lib/config/constant.dart
@@ -1,10 +1,9 @@
-
 import 'package:learn/config/env_config.dart';
 
-class Constant{
+class Constant {
   static final String apiUrl = EnvConfig.apiUrl;
 
-  static const int successText = 200;
+  static const int successText = 201;
 
   static const int errorText = 404;
 
